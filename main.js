642f5b4dfc3e28efbc3da47872c58391beb9aa22
Cederlund1999 <cederlund1999@gmail.com>
2021-02-22 15:53:29 +0100

    While loop skapar karaktärslista

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-async function apiTestCall(){
+window.onload = async function createCharacterList(){
     const response = await fetch(`https://swapi.dev/api/people/`);
     const responseData = await response.json()
     console.log(responseData);  
+    
+    let i = 0;
+    let x = 0;
+    while (i < 10) {
+    let li = document.createElement("li");
+    let text = document.createTextNode(responseData.results[x].name);
+    li.appendChild(text);
+    document.getElementById("myUl").appendChild(li);
+    i++;
+    x++;
+    
+}
+
 }
