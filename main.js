7df26248de674fe7d575be1bb1c3a5ee5cf4047c
Cederlund1999 <cederlund1999@gmail.com>
2021-02-24 14:16:50 +0100

    githubpages fel test

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";   
    onload = async function createCharacterList(){
-    const response = await fetch(`http://swapi.dev/api/people/?page=1`);
+    const response = await fetch(`https://swapi.dev/api/people/`);
     const responseData = await response.json()
     console.log(responseData);  
     
@@ -15,9 +15,7 @@
     x++;
     
 }
-
 }
-
 document.getElementById('showMoreClick').onclick = async function(){
     const response = await fetch(`http://swapi.dev/api/people/?page=2`)
     const data = await response.json();
